8076237dda7213757e30665c7c2044f580e215cf
akshuu27 <akshujagdale2003@gmail.com>
2024-11-09 10:53:08 +0530

    fs.js

---
+++ fs.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const http =require ("http")
+
+//  http.createServer((req, res)=>{
+    // fs.readFile('index.js', (err, data)=>{
+//         res. writeHead(200, {'content-type':' text/html'})
+//         res.write(data)
+//         return res .end()
+    // })
+
+// append file 
+// fs.appendFile ('index.html',  '<h1>this is append file data</h1>', (err, data)=>{
+//     if (err) throw err 
+//     console.log("file updated");
+//     return res.end()
+// })
+
+ //  delete file 
+
+//  fs.unlink ('index.html', (err) =>{
+//   if (err) throw err
+//   console.log("file deleted");
+//   return res.end()
+//  })
+
+  //  let fileexist = fs.
+
+  // }).listen(5000)
+
+
